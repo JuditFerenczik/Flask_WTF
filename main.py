eeddcebc03a00a42185bf78868221c2a740ac402
Judit Ferenczik <jferenczik@gmail.com>
2022-11-14 22:23:05 +0100

    POST added

--- main.py
+++ main.py
@@ -22,7 +22,12 @@ def home():
 @app.route("/login", methods=["GET","POST"])
 def login():
     form = MyForm()
-    form.validate_on_submit()
+    if form.validate_on_submit():
+        print(form.name.data)
+        if (form.name.data == "admin@gmail.com") and (form.password.data =="12345678"):
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=form)
 
 
