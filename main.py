4ce041c4635343b280b72156a233e182a8e0d91b
Judit Ferenczik <jferenczik@gmail.com>
2022-11-15 08:40:32 +0100

    bootstrap app added

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "some secret string"
 
 
